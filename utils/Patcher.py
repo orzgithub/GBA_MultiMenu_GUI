2210df7893e97d88868f8337124a1933b5cd38a2
orzgithub <49035592+orzgithub@users.noreply.github.com>
2024-04-01 22:55:28 +0800

    Fixed the issues when using with different code environment.

--- utils/Patcher.py
+++ utils/Patcher.py
@@ -3,21 +3,33 @@
 from lib import batteryless_patch
 from lib import gba_patch
 
+import locale
+
 
 def batteryless_patcher(
     rom_path: str, out_path: str
 ) -> int:  # 0: Done 1: Failed but ok 2: Failed and broken
     print(end="")  # To make the patch result show in terminal at once.
-    return batteryless_patch.patch(rom_path, out_path)
+    return batteryless_patch.patch(
+        rom_path.encode(locale.getpreferredencoding()),
+        out_path.encode(locale.getpreferredencoding()),
+    )
 
 
 def sram_patcher(rom_path: str, out_path: str) -> int:  # 0: Done 1: Failed
     print(end="")
-    return gba_patch.sram_patch(rom_path, out_path)
+    return gba_patch.sram_patch(
+        rom_path.encode(locale.getpreferredencoding()),
+        out_path.encode(locale.getpreferredencoding()),
+    )
 
 
 def ips_patcher(
     rom_path: str, ips_path: str, out_path: str
 ) -> int:  # 0: Done 1: Failed
     print(end="")
-    return gba_patch.ips_patch(rom_path, ips_path, out_path)
+    return gba_patch.ips_patch(
+        rom_path.encode(locale.getpreferredencoding()),
+        ips_path.encode(locale.getpreferredencoding()),
+        out_path.encode(locale.getpreferredencoding()),
+    )
