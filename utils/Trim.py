eb2affa34ac2299125a770c7b45465a6344f5484
orzgithub <49035592+orzgithub@users.noreply.github.com>
2024-04-16 13:48:38 +0800

    Added output when using trim and align. Allow to use different align size.

--- utils/Trim.py
+++ utils/Trim.py
@@ -1,25 +1,29 @@
-def align(rom_path: str, out_path: str):
+def align(rom_path: str, out_path: str, align_size: int = 16):
     with open(rom_path, "rb") as rom:
         rom_data = rom.read()
-    if len(rom_data) % 16:
-        rom_data += (16 - (len(rom_data) % 16)) * b"\0"
+    print(f"Starting to align {rom_path} to {align_size}B.")
+    if len(rom_data) % align_size:
+        rom_data += (align_size - (len(rom_data) % align_size)) * b"\0"
     with open(out_path, "wb") as out:
         out.write(rom_data)
 
 
-def trim(rom_path: str, out_path: str):
+def trim(rom_path: str, out_path: str, align_size: int = 16):
     with open(rom_path, "rb") as rom:
         rom_data = rom.read()
-    if len(rom_data) % 16:
-        rom_data += (16 - (len(rom_data) % 16)) * b"\0"
+    print(f"Starting to trim {rom_path}.")
+    print(f"Size before trim: {len(rom_data)/1024/1024}MB.")
+    if len(rom_data) % align_size:
+        rom_data += (align_size - (len(rom_data) % align_size)) * b"\0"
     try:
-        trim_data = rom_data[-16:]
-        assert trim_data in [16 * b"\x00", 16 * b"\xFF"]
-        cur_trim = len(rom_data) - 16
-        while rom_data[cur_trim : cur_trim + 16] == trim_data:
-            cur_trim -= 16
-        rom_data = rom_data[: cur_trim + 16]
+        trim_data = rom_data[-align_size:]
+        assert trim_data in [align_size * b"\x00", align_size * b"\xFF"]
+        cur_trim = len(rom_data) - align_size
+        while rom_data[cur_trim : cur_trim + align_size] == trim_data:
+            cur_trim -= align_size
+        rom_data = rom_data[: cur_trim + align_size]
+        print(f"Size after trim: {len(rom_data)/1024/1024}MB.")
     except:
-        pass
+        print(f"Can't trim {rom_path} and it's not needed to be trimmed.")
     with open(out_path, "wb") as out:
         out.write(rom_data)
