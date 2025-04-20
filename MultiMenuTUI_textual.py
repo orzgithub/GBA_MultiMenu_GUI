0ffc3151c98b1c97756bb74ef76602be433740e2
orzgithub <49035592+orzgithub@users.noreply.github.com>
2025-04-20 19:54:12 +0800

    Remove Textual-based TUI. Seems that actually nobody using them, and they were break because of the update of the textual.

--- MultiMenuTUI_textual.py
+++
@@ -1,8 +0,0 @@
-from textual_ui import MenuBuilderTUI
-from resources_src import Config, I18n
-
-if __name__ == "__main__":
-    config = Config.Config()
-    lang = eval(f"I18n.{config.lang}")
-    ui = MenuBuilderTUI.MenuBuilderTUI(lang)
-    ui.run()
